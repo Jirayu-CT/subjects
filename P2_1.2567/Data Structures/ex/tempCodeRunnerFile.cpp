bff9d90573de1332de44fc160719de366ea77e49
Lucibel <125994181+Jirayu-CT@users.noreply.github.com>
2024-07-02 21:45:34 +0700

    2/7/2567

---
+++ P2_1.2567/Data Structures/ex/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+Product *product1 = new Product{735011, "Iphone11", 25000.25};
+    // Product *product2 = new Product{735013, "Iphone13", 30000.50};
+    // Product *product3 = new Product{735015, "Iphone15", 35000.75};
+
+    // product = product1;
+    // product1->next = product2;
+    // product2->next = product3;
