d31554470560e892a8a298dbb0f88a559f4a2a66
Lucibel <125994181+Jirayu-CT@users.noreply.github.com>
2024-07-24 22:44:04 +0700

    OOP3 EX.3.2
    24/7/2567

---
+++ P2_1.2567/advanced_programming/classroom/oop/OOP3/OOP3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ractangle ractangle1 = new Ractangle(width: 50, height: -5);
+            
+            ractangle1.SetWidth(-5);
+            ractangle1.SetHeight(50);
+
+            Console.WriteLine("Width: " + ractangle1.GetWidth()); 
+            Console.WriteLine("Height: " + ractangle1.GetHeight());
+            Console.WriteLine("Ractangle = " + ractangle1.GetArea());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P2_1.2567/advanced_programming/classroom/oop/OOP3/OOP3/Ractangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    internal class Ractangle
+    {
+        private double _width;
+        private double _height;
+        private double _area;
+
+
+        public Ractangle(double width, double height)
+        {
+            _width = CheckNumber(width);
+            _height = CheckNumber(height);
+        }
+
+        public double CheckNumber(double value)
+        {
+            if (value >= 0)
+            {
+                return value;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+
+        public double GetWidth()
+        {
+            
+            return CheckNumber(_width);
+        }
+
+        public void SetWidth(double width)
+        {
+            _width = CheckNumber(width);
+        }
+
+
+        public double GetHeight()
+        {
+            return CheckNumber(_height);
+        }
+
+        public void SetHeight(double height)
+        {
+            _height = CheckNumber(height);
+        }
+
+
+        public double GetArea()
+        {
+            _area = _height * _width;
+            return _area;
+        }
+    }
+}
